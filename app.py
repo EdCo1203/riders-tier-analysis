e2f8841867f7d6d4741c41c8cdf57d85ccabeff2
Edgar <ejccmujica@gmail.com>
2026-04-13 19:16:35 +0200

    Arreglo de filtro de mensajes

--- app.py
+++ app.py
@@ -299,7 +299,9 @@ with tab2:
 
     solo_con_fallos = st.checkbox("Mostrar solo riders con fallos", value=True)
 
-    riders_msg = df45_sorted[df45_sorted["_n_fallos"] > 0] if solo_con_fallos else df45_sorted
+    riders_msg = df45_sorted[
+        (df45_sorted["Tier"].isin(filtro_tier)) &
+        (df45_sorted["_n_fallos"] > 0)] if solo_con_fallos else df45_sorted
 
     for _, rider in riders_msg.iterrows():
         fallos = rider["_fallos"]
